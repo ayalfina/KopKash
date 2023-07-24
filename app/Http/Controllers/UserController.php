090d0735e07fb04cbcfcd7471f4bedb85b600010
kingfeb07 <bagusfebryanto34@gmail.com>
2023-07-24 17:23:00 +0700

    dimas rafly push

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Inventory;
+use Illuminate\Http\Request;
+use App\Exports\UserExport;
+use Maatwebsite\Excel\Facades\Excel;
+use Barryvdh\DomPDF\Facade\Pdf;
+use App\Http\Controllers\Controller;
+
+
+class UserController extends Controller
+{
+    public function data()
+    {
+        return DataTables::of(User::query())->toJson();
+    }
+
+    public function index()
+    {
+        return view('stock.index');
+    }
+    public function export_excel()
+	{
+		return Excel::download(new UserExport, 'product.xlsx');
+	}
+    public function cetak_pdf()
+    {
+        $Inventory = Inventory::all();
+
+        $pdf = PDF::loadview('pages.user_pdf', ['Inventory' => $Inventory]);
+        return $pdf->download('laporan-user.pdf');
+    }
+
+}
